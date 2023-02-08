1ac1b1318e3a6dd0ae7d08c0449abf980b9b37c4
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-08 23:36:39 +0300

    Math operations 2

---
+++ basics/js/script_8.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 2;
+alert("Задание 1.1 " + (a+b));
+alert("Задание 1.2 " + (a-b));
+alert("Задание 1.3 " + (a*b));
+alert("Задание 1.4 " + (a/b));
+
+a = 10;
+b = 5;
+let result = a+b;
+alert("Задание 2 " + (result));
+
+a = 1;
+b = 2;
+let c = 3;
+alert("Задание 3 " + (a + b + c));
+
+a = 10;
+b = 5;
+c = 10 - 5;
+
+let d = 7;
+result = c + d;
+alert("Задание 4 " + result);
