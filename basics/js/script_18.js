bdfcd6d8dd6310e1aa102b135ac4db1865889c12
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-09 22:52:51 +0300

    Special values

---
+++ basics/js/script_18.js
@@ -0,0 +1,18 @@
+let a;
+alert(a);
+
+let b = null;
+alert(b);
+
+let c = true;
+alert(c);
+
+let d = false;
+alert(d);
+
+let str1 = 'haha';
+let str2 = 'gaga';
+alert(str1 * str2);
+
+alert(10 / 0);
+alert(-10 / 0);
