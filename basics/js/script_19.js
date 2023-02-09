f5446352eaf85df762125dbf2a7a38ac0aaaa5bb
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-09 23:01:47 +0300

    Console

---
+++ basics/js/script_19.js
@@ -0,0 +1,13 @@
+let a = 10;
+console.log(a);
+
+let b = 123, c = 456, d = 789;
+console.log(`b = ${b}, c = ${c}, d = ${d}`);
+
+console.log(123);
+console.log("lala");
+console.log(true);
+console.log(undefined);
+console.log(null);
+
+console.lag("error");
