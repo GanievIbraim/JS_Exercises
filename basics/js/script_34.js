1715e60384858169e6768a943e03d83d244d2927
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-12 17:32:45 +0300

    document.write()

---
+++ basics/js/script_34.js
@@ -0,0 +1,5 @@
+document.write('The text is output using document.write()');
+document.write('<br>> <i>Text in italics</i>');
+let str = 'Task 3'
+document.write(`<br>> <i>${str}</i>`);
+document.write('<br>> 1 <br>> 2 <br>> 3 <br>> 4 <br>> 5');
