120a71c2ca6f86f8fdbf2b3d4dfead6e3e9a9c7f
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-12 15:02:18 +0300

    String

---
+++ basics/js/script_24.js
@@ -0,0 +1,10 @@
+let a = 120;
+let b = 60;
+alert(String(a) + String(b));
+
+let num = 618127419024;
+alert(String(num).length);
+
+let num1 = 456;
+let num2 = 8905;
+alert(String(num1).length + String(num2).length);
