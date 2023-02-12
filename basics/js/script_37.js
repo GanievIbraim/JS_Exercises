c83eb72002a325ac217a7f39d232552cc91e5928
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-12 18:20:56 +0300

    Practice 2

---
+++ basics/js/script_37.js
@@ -0,0 +1,22 @@
+let r = 20;
+let s = 3.14 * 20**2
+alert(s);
+
+let a = 10;
+s = a*a;
+alert(s);
+
+a = 12;
+b = 21;
+alert(a*b);
+
+a = 33;
+b = 66;
+p = (a+b)*2;
+alert(p);
+
+let tc =  55;
+alert((9/5)*tc + 32);
+
+let tf = 97;
+alert((5/9) * (tf - 32));
