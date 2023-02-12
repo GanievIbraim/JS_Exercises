6995bf47ce82e541657bde39bdf024b20a01b6af
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-12 15:54:58 +0300

    Strings with numbers

---
+++ basics/js/script_29.js
@@ -0,0 +1,11 @@
+let str = '12345';
+alert(+str[0] + (+ str[1]) + (+ str[2]) + (+ str[3]) + (+ str[4]));
+
+str = String(12345);
+alert(+str[0] + (+ str[1]) + (+ str[2]) + (+ str[3]) + (+ str[4]));
+
+str = String(12345);
+alert(+str[0] * (+ str[1]) * (+ str[2]) * (+ str[3]) * (+ str[4]));
+
+str = String(12345);
+alert(str[4] + str[3] + str[2] + str[1] + str[0]);
