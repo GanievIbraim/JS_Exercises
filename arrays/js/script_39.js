8e07186ea26fc85786148ccbb8e63f7ef5907ae4
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-12 22:22:11 +0300

    Array 4

---
+++ arrays/js/script_39.js
@@ -0,0 +1,15 @@
+let arr = [1, 2, 3];
+alert(arr[0]);
+alert(arr[1]);
+alert(arr[2]);
+
+let arr1 = [1, 2, 3];
+alert(arr1[0]);
+alert(arr1[1]);
+alert(arr1[2]);
+
+let arr2 = [1, 2, 3];
+alert(arr2[0] + arr2[1] + arr2[2]);
+
+let arr3 = ['a', 'b', 'c', 'd'];
+alert(`${arr3[0]} + ${arr3[1]} + ${arr3[2]} + ${arr3[3]}`)
---
+++ arrays/js/script_40.js
@@ -0,0 +1,5 @@
+let arr1 = ['a', 'b', 'c', 212, 'qwewesd'];
+console.log(arr1.length);
+
+let arr2 = ['a', 'b', 'c', 212, 'qwewesd'];
+console.log(arr2[arr2.length - 1]);
