c62cb4375661cfd971fb767655f0656fe4f573c5
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-12 17:44:59 +0300

    Error

---
+++ basics/js/script_35.js
@@ -0,0 +1,28 @@
+let num1 = 1;
+let num2 = 2;
+console.log('сумма: ' + (num1 + num2));
+
+
+let a = 1;
+let b = 2;
+let c = 3;
+console.log(a + b + c);
+
+
+let num_3 = '123';
+let sum = +num_3[0] + (+num_3[1]) + (+num_3[2]);
+console.log(sum);
+
+
+let num_4 = String(123);
+console.log(num_4[0]);
+
+
+let aa = 0;
+console.log(++aa);
+
+let aaa = 1;
+let bbb = 2;
+let ccc = 3;
+
+console.log(aaa + bbb + ccc);
