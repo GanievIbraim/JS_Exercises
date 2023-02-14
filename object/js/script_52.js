1c522c818af20a6ebe1479768729202e41647888
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-14 21:29:22 +0300

    Key Restrictions

---
+++ object/js/script_52.js
@@ -0,0 +1,13 @@
+let obj = {
+	'1a': 1, // Обязательно
+	'b2': 2, // Не обязательно
+	'c-c': 3, // Обязательно
+	'd 4': 4, // Обязательно
+	'e5': 5 // Не обязательно
+};
+
+console.log(obj['1a']);
+console.log(obj.b2);
+console.log(obj['c-c']);
+console.log(obj['d 4']);
+console.log(obj.e5);
