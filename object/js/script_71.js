2daa6e3b416f637d0172ba72f8d5cf8eb8a0576e
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-17 23:21:17 +0300

    Finding errors

---
+++ object/js/script_71.js
@@ -0,0 +1,20 @@
+// Задание 1
+let obj_1 = {x: 1, y: 2, z: 3};
+console.log(obj_1.x);
+
+// Задание 2
+let obj_2 = {x: 1, y: 2, z: 3};
+let key = 'x';
+
+console.log(obj_2[key]);
+
+// Задание 3
+let obj_3 = {x: 1, y: 2, z: 3};
+let sum = obj_3.x + obj_3.y + obj_3.x;
+
+console.log(obj_3);
+
+// Задание 4
+let obj_4 = {x: 1, y: 2, z: 3};
+let keys = Object.keys(obj_4);
+console.log(keys.length);
