13909272dab0f1a4e2f77423fec4efaafa8e3eb3
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-17 18:58:52 +0300

    Calculated properties

---
+++ object/js/script_61.js
@@ -0,0 +1,21 @@
+let key = 'x';
+
+let obj_1 = {
+	[key]: 1,
+	y: 2,
+	z: 3
+};
+
+console.log(obj_1);
+
+
+let key1 = 'x';
+let key2 = 'y';
+let key3 = 'z';
+
+let obj_2 = {
+	[key1]: 5,
+	[key2]: 6,
+	[key3]: 7
+};
+console.log(obj_2);
