de8a55e707a630847f6235388ac324db172972ab
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-17 22:03:11 +0300

    Objects and primitives

---
+++ object/js/script_67.js
@@ -0,0 +1,7 @@
+// {x: 1, y: 2, z: 3} object объект
+// 1 number примитив
+// [1, 2, 3] object объект
+// 2 number примитив
+// [1, 2, 3] object объект
+// 2 number примитив
+// Task 7: number, boolean, string, bigint, symbol, undefined, null
