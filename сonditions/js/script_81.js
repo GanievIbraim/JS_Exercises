c002355411638fce96eecb809cc8a9f63d4a97bb
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-21 02:56:43 +0300

    Logical AND

---
+++ сonditions/js/script_81.js
@@ -0,0 +1,23 @@
+let num = 2;
+
+if (num > 0 && num < 5) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let num2 = 21;
+if (num2 >= 10 || num2 <= 20) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+let num3 = 0;
+let num4 = 3;
+
+if (num3 <= 1 && num4 >= 3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
