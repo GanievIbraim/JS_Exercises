79da272ddefa26046e8714159972c92be2bc8c0c
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-21 02:10:22 +0300

    Operators more and less

---
+++ сonditions/js/script_73.js
@@ -0,0 +1,23 @@
+if (test > 10) {
+	console.log('+++');
+} else {
+	console.log('test не больше 10');
+}
+
+if (test < 10) {
+	console.log('test меньше 10');
+} else {
+	console.log('---');
+}
+
+if (test >= 10) {
+	console.log('+++');
+} else {
+	console.log('test не больше и не равен 10');
+}
+
+if (test <= 10) {
+	console.log('test меньше или равен 10');
+} else {
+	console.log('---');
+}
