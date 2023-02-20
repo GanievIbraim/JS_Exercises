ede775a47c86b265eb3c623f2b3cbf2fcc1b3320
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-21 02:20:55 +0300

    Comparison of variables

---
+++ сonditions/js/script_76.js
@@ -0,0 +1,16 @@
+let test1 = 33;
+let test2 = 214;
+let test3 = 343;
+let test4 = 384;
+
+if (test2 > test1) {
+	console.log('test2 больше test1');
+} else {
+	console.log('test2 меньше test1');
+}
+
+if (test2 == test1) {
+	console.log('test3 равен test4');
+} else {
+	console.log('test3 не равен test4');
+}
