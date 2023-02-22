92e92d6c389acf360d9dd832377df2c9b0062377
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-23 00:26:53 +0300

    Grouping conditions

---
+++ сonditions/js/script_84.js
@@ -0,0 +1,27 @@
+// TASK 1
+let num = 3;
+		
+if ((num > 5 && num < 10) || num == 20) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// TASK 2
+let num2 = 3;
+		
+if (num2 > 5 || (num2 > 0 && num2 < 3)) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// TASK 2
+let num3 = 3;
+		
+if ((num3 == 9 )|| (num3 > 10 && num3
+	< 20) || (num3 > 20 && num3 < 30)) { 
+	console.log('+++');
+} else {
+	console.log('---');
+}
