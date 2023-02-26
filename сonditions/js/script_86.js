7e38cb0b0be03c91ab6bff91c01604572a462226
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-26 13:20:18 +0300

    Conditions with Boolean values

---
+++ сonditions/js/script_86.js
@@ -0,0 +1,17 @@
+// TASK 1
+let test1 = true;
+
+if (test1 === true) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// TASK 2
+let test2 = true;
+
+if (test2 === false) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
