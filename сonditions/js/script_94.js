aec44a689e39e48c31bbfca99e6941a859be5f90
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-27 00:04:43 +0300

    Else if construction

---
+++ сonditions/js/script_94.js
@@ -0,0 +1,12 @@
+// TASK 1, 2
+let day = 115;
+
+if (1 <= day && day <=  10) {
+	console.log('Первая декада');
+} else if (11 <= day && day <=  20) {
+	console.log('Вторая декада');
+} else if (21 <= day && day <=  31) {
+	console.log('Третья декада');
+} else {
+	console.log('Неверное значение');
+}
