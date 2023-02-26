69d3354e933608fd6a82790f1e7e1eef4720079d
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-26 13:34:09 +0300

    Abbreviated form

---
+++ сonditions/js/script_89.js
@@ -0,0 +1,26 @@
+// TASK 1
+let test1 = true;
+
+if (!test1) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// TASK 2
+let test2 = true;
+
+if (!test2) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
+
+// TASK 2
+let test3 = true;
+
+if (test3) {
+	console.log('+++');
+} else {
+	console.log('---');
+}
