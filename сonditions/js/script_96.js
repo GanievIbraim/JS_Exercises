a0748a5f6ff31167541ee539ba57b80bb7c223c5
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-27 00:27:27 +0300

    Switch-case design

---
+++ сonditions/js/script_96.js
@@ -0,0 +1,17 @@
+// TASK 1
+let lang = 'de';
+
+switch (lang) {
+	case 'ru':
+		console.log('рус');
+	break;
+	case 'en':
+		console.log('анг');
+	break;
+	case 'de':
+		console.log('нем');
+	break;
+	default:
+		console.log('язык не поддерживается');
+	break;
+}
