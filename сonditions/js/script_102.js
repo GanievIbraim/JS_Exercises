887ce057e98f6dc6042e01dddb7bceb8d46e6117
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-27 11:15:31 +0300

    Nuances of the scope

---
+++ сonditions/js/script_102.js
@@ -0,0 +1,55 @@
+// TASK 1
+let age = 17;
+let adult;
+
+if (age >= 18) {
+	adult = true;
+} else {
+	adult = false;
+}
+
+console.log(adult);
+
+// TASK 2
+let age2 = 17;
+let adult2;
+
+if (age2 >= 18) {
+	adult2 = true;
+} else {
+	adult2 = false;
+}
+
+console.log(adult2);
+
+// TASK 3
+let age3 = 17;
+let res;
+
+if (age3 >= 18) {
+	if (age3 <= 23) {
+		res = 'от 18 до 23';
+	} else {
+		res = 'больше 23';
+	}
+} else {
+	res = 'меньше 18';
+}
+
+console.log(res);
+
+// TASK 4
+let age4 = 19;
+let res2;
+
+if (age4 >= 18) {
+	if (age4 <= 23) {
+		res2 = 'от 18 до 23';
+	} else {
+		res2 = 'больше 23';
+	}
+} else {
+	res2 = 'меньше 18';
+}
+
+console.log(res2);
