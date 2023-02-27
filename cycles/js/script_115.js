f32b21b8e532ad4874bcdea0a54388630028d67e
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-27 20:54:48 +0300

    For loop for arrays

---
+++ cycles/js/script_115.js
@@ -0,0 +1,25 @@
+// TASK 1
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log('TASK 1');
+for (let i = 0; i < arr.length; i++) {
+	console.log(arr[i]);
+}
+
+// TASK 2
+console.log('TASK 2');
+for (let i = 1; i < arr.length-1; i++) {
+	console.log(arr[i]);
+}
+
+// TASK 3
+console.log('TASK 3');
+for (let i = arr.length-1; i >= 0 ; i--) {
+	console.log(arr[i]);
+}
+
+// TASK 3
+console.log('TASK 4');	
+for (let i = 0; i <= arr.length-1; i++) {
+	console.log(arr[i]);
+}
