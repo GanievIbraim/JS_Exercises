2111bcdbdd209b4906360c6a353f6902d07c8a81
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-27 20:13:51 +0300

    For loop

---
+++ cycles/js/script_111.js
@@ -0,0 +1,33 @@
+// TASK 1
+console.log('TASK 1');
+for (let i = 1; i <= 100; i++) {
+	console.log(i);
+}
+
+// TASK 2
+console.log('TASK 2');
+for (let i = 11; i <= 33; i++) {
+	console.log(i);
+}
+
+// TASK 3
+console.log('TASK 3');
+for (let i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
+    }
+}
+
+// TASK 4
+console.log('TASK 4');
+for (let i = 1; i <= 99; i++) {
+    if (i % 2 == 1) {
+        console.log(i);
+    }
+}
+
+// TASK 5
+console.log('TASK 5');
+for (let i = 100; i >= 0; i--) {
+    console.log(i);
+}
