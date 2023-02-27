226719b96e405b6c9e6a43c418846f1193b5d954
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-27 11:37:06 +0300

    Checking string characters

---
+++ сonditions/js/script_105.js
@@ -0,0 +1,22 @@
+// TASK 1
+let str = 'babvcadxa';
+if (str[0] == 'a') {
+	console.log('Cтрока начинается на символ "a"');
+} else {
+	console.log('Cтрока не начинается на символ "a"');
+}
+
+// TASK 2
+if (str[str.length - 1] == 'x') {
+	console.log('Cтрока начинается на символ "x"');
+} else {
+	console.log('Cтрока не начинается на символ "x"');
+}
+
+// TASK 3
+if (str[0] == 'a' || str[0] == 'b') {
+	console.log('Cтрока начинается на символ "a" или "b"');
+} else {
+	console.log('Cтрока не начинается на символ "a" или "b"');
+}
+
