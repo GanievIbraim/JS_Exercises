0a22eedb6b14c5b2beb274eafa4aa0d70789b1c2
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-27 20:42:57 +0300

    While

---
+++ cycles/js/script_114.js
@@ -0,0 +1,29 @@
+// TASK 1
+console.log('TASK 1');
+let i = 1;
+
+while (i <= 100) {
+	console.log(i);
+	i++;
+}
+
+// TASK 2
+console.log('TASK 2');
+let y = 11;
+
+while (y <= 33) {
+	console.log(y);
+	y++;
+}
+
+// TASK 3
+console.log('TASK 3');
+let num = 23;
+let count = 0;
+
+while (num <= 1000) {
+	num *= 3;
+	count++;
+}
+console.log('Количество итераций', + count);
+console.log(num);
