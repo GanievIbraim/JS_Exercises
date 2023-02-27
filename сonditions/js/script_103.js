bb4e9046079420c4fc412cd31fc92961dba079b5
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-27 11:19:16 +0300

    Checking the parts of the hour

---
+++ сonditions/js/script_103.js
@@ -0,0 +1,14 @@
+// TASK 1
+let min = 10;
+
+if (min >= 0 && min <= 19) {
+	console.log('1 четверть');
+}
+
+if (min >= 20 && min <= 39) {
+	console.log('2 четверть');
+}
+
+if (min >= 40 && min <= 59) {
+	console.log('3 четверть');
+}
