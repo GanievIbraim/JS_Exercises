763c352b049379cecbb482f1de1a713399a3de5f
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-02-27 12:45:51 +0300

    Checking the remainder of the division

---
+++ сonditions/js/script_107.js
@@ -0,0 +1,15 @@
+// TASK 1
+let num = 120;
+
+if (num % 2 == 0) {
+	console.log('Четное число');
+} else {
+	console.log('Нечетное число');
+}
+
+// TASK 2
+if (num % 3 == 0) {
+	console.log('Число кратно 3');
+} else {
+	console.log('Число не кратно 3');
+}
