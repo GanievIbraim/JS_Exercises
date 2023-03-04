d25c896c382822345771293e19440512ce804a15
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-05 00:43:10 +0300

    Formation of strings

---
+++ cycles/js/script_119.js
@@ -0,0 +1,35 @@
+// TASK 1
+let str = '';
+
+for (let i = 1; i <= 5; i++) {
+	str += '-';
+}
+
+console.log(str);
+
+// TASK 2
+str = '';
+
+for (let i = 1; i <= 9; i++) {
+	str += i;
+}
+
+console.log(str);
+
+// TASK 3
+str = '';
+
+for (let i = 9; i >= 1; i--) {
+	str += i;
+}
+
+console.log(str);
+
+// TASK 4
+str = '';
+
+for (let i = 1; i <= 9; i++) {
+	str += i + '-';
+}
+
+console.log(str);
