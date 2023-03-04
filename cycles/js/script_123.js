8cfe62dcbff29ba0c348aab0f3c855b6952acb99
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-05 01:15:16 +0300

    Nested loops

---
+++ cycles/js/script_123.js
@@ -0,0 +1,13 @@
+// TASK 1
+for (let i = 1; i <= 9; i++) {
+	for (let j = 1; j <= 3; j++) {
+		document.write(i);
+	}
+}
+
+// TASK 2
+for (let i = 1; i <= 3; i++) {
+	for (let j = 1; j <= 3; j++) {
+		document.write('\n' + i + j + ' ');
+	}
+}
