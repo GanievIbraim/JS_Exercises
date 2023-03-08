40e3e79e76fe4cec593bfca021aace5e6429e186
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-08 17:01:59 +0300

    Changing an array in a loop

---
+++ cycles/js/script_127.js
@@ -0,0 +1,22 @@
+// TASK 1
+let arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+	arr[i] = arr[i] ** 2;
+}
+
+console.log(arr); 
+
+// TASK 2
+for (let i = 0; i < arr.length; i++) {
+	arr[i] = arr[i] - 1;
+}
+
+console.log(arr); 
+
+// TASK 2
+for (let i = 0; i < arr.length; i++) {
+	arr[i] = arr[i] + 10;
+}
+
+console.log(arr); 
