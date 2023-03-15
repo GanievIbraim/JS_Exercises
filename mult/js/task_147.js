16bf6a2b77f8ea0b749a21b84248c797ad8d1bcb
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-15 20:08:52 +0300

    Array of objects

---
+++ mult/js/task_147.js
@@ -0,0 +1,36 @@
+// TASK 1
+let employees = [
+	{
+		name: 'name1',
+		salary: 300,
+		age: 28,
+	},
+	{
+		name: 'name2',
+		salary: 400,
+		age: 29,
+	},
+	{
+		name: 'name3',
+		salary: 500,
+		age: 30,
+	},
+	{
+		name: 'name4',
+		salary: 600,
+		age: 31,
+	},
+	{
+		name: 'name5',
+		salary: 700,
+		age: 32,
+	},
+];
+
+let sum = 0;
+for (let user of employees) {
+	if (user.age >= 30){
+		sum += user.salary;
+	}
+}
+console.log(sum);
