6f5bb476dfee74b302f0192d0c4fec912a96cd61
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-15 22:52:04 +0300

    Function Parameters

---
+++ user_func/js/script_157.js
@@ -0,0 +1,18 @@
+// TASK 1
+func(2);
+
+function func(num) {
+	console.log(num * num);
+}
+
+// TASK 2
+func_2(-10);
+
+function func_2(num) {
+	if (num >= 0){
+        console.log('+++');
+    }
+    else {
+        console.log('---');
+    }
+}
