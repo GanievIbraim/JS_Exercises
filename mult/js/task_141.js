3ab2356e17550a886933aa22752993a8ca238d18
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-15 17:52:54 +0300

    Problems

---
+++ mult/js/task_141.js
@@ -0,0 +1,76 @@
+// TASK 1
+let arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr[i] = []
+	for (let j = 1; j <= 5; j++) {
+		arr[i].push(j);
+	}
+}
+
+console.log(arr);
+
+// TASK 2
+arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr[i] = []; // arr[i] = '';
+	
+	for (let j = 1; j <= 5; j++) {
+		arr[i].push(j);
+	}
+}
+
+console.log(arr);
+
+// TASK 3
+arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr[i] = []; // arr[i];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr[i].push(j);
+	}
+}
+
+console.log(arr);
+
+// TASK 4
+arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr[i] = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr[i].push(j);
+	}
+}
+
+console.log(arr);
+
+// TASK 5
+arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr[i] = []; // arr = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr[i].push(j);
+	}
+}
+
+console.log(arr);
+
+// TASK 6
+arr = [];
+
+for (let i = 0; i < 3; i++) {
+	arr[i] = [];
+	
+	for (let j = 1; j <= 5; j++) {
+		arr[i].push(j); // arr.push(j);
+	}
+}
+
+console.log(arr);
