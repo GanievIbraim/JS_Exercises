3e08d5bb673884b7059f3b153fc955c6c0c8dd97
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-15 22:01:33 +0300

    Introduction

---
+++ user_func/js/script_156.js
@@ -0,0 +1,15 @@
+// TASK 1
+func();
+
+function func() {
+	console.log('Ibraim');
+}
+
+// TASK 2
+func_2();
+
+function func_2() {
+	for (let i = 0; i <= 100; i++){
+        console.log(i);
+    }
+}
