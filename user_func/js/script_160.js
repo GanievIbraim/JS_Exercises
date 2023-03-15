71796db8e3aa53faf0c8be70248ce21c2a845734
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-15 23:03:04 +0300

    Optional parameters

---
+++ user_func/js/script_160.js
@@ -0,0 +1,17 @@
+// TASK 1
+function func(num = 5) {
+	console.log(num * num);
+}
+
+func(2);
+func(3);
+func();
+
+// TASK 2
+function func2(num1 = 0, num2 = 0) {
+	console.log(num1 + num2);
+}
+
+func2(2, 3);
+func2(3);
+func2();
