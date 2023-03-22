fa3e552ceba1540b4f2a2c204580801fdba29628
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-22 18:37:16 +0300

    Matching names with parameters

---
+++ user_func_2/js/task_177.js
@@ -0,0 +1,26 @@
+// TASK 1
+// function func(num) {
+// 	num = 2;
+// }
+
+// let num = 1;
+// func(num);
+// console.log(num); // 1
+
+// TASK 2
+// function func() {
+// 	num = 2;
+// }
+
+// let num = 1;
+// func();
+// console.log(num); // 2
+
+// TASK 3
+// function func() {
+// 	let num = 2;
+// }
+
+// let num = 1;
+// func();
+// console.log(num); // 1
