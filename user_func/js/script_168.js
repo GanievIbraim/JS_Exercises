944618dfee4aafb530341c48bc4daf2cef0ff810
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-22 17:16:13 +0300

    Logical operators without if

---
+++ user_func/js/script_168.js
@@ -0,0 +1,19 @@
+// TASK 1
+function func(a, b) {
+	return (a == b)
+}
+
+// TASK 2
+function func(a, b) {
+	return (a != b)
+}
+
+// TASK 3
+function func(a, b) {
+	return (a + b >= 10)
+}
+
+// TASK 4
+function func(num) {
+	return (num >= 0)
+}
