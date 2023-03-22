b2dce68356c4968d6ed6daaca10509a56dcc472b
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-22 09:09:08 +0300

    return

---
+++ user_func/js/script_161.js
@@ -0,0 +1,15 @@
+// TASK 1
+function func(num) {
+	return num * num * num
+}
+
+let res = func(3)
+console.log(res);
+
+// TASK 2
+function func2(num) {
+	return num**(1/2)
+}
+
+res = func2(4) + func2(9) 
+console.log(res);
