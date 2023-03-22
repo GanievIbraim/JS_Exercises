0987d1ec34abea52ae62e83dd894b0e062450e7d
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-22 18:40:32 +0300

    Parameters-objects

---
+++ user_func_2/js/task_178.js
@@ -0,0 +1,23 @@
+// TASK 1
+function func(num) {
+	num = 2;
+}
+let num = 1;
+func(num);
+console.log(num);
+
+// TASK 2
+function func1() {
+	num1 = 2;
+}
+let num1 = 1;
+func1();
+console.log(num1);
+
+// TASK 3
+function func2() {
+	let num2 = 2;
+}
+let num2 = 1;
+func2();
+console.log(num2);
