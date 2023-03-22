baa79c9270a275c5eeefe8e6c7d07de290525b1b
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-22 17:54:50 +0300

    Local variables

---
+++ user_func_2/js/task_173.js
@@ -0,0 +1,15 @@
+// TASK 1
+// function func() {
+// 	let num = 5;
+// 	return num;
+// }
+
+// console.log(num); // ОШИБКА
+
+//  TASK 2
+// function func() {
+// 	let num = 5;
+// 	return num;
+// }
+
+// console.log(func()); // 5
