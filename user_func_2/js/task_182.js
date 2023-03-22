5a470b3c8f307e5419621abc75a54524d2b020bc
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-22 19:06:29 +0300

    Nuances of functional expressions

---
+++ user_func_2/js/task_182.js
@@ -0,0 +1,41 @@
+// TASK 1
+function func() {
+	console.log('!');
+}
+
+// TASK 2
+let func = function () {
+	console.log('!');
+}
+
+// TASK 3
++function () {
+	console.log('!');
+}
+
+// TASK 4
+! function func() {
+	console.log('!');
+}
+
+// TASK 5
+-function func() {
+	console.log('!');
+}
+
+// TASK 6
+1 + function func() {
+	console.log('!');
+}
+
+// TASK 7
+(function func() {
+	console.log('!');
+})
+
+// TASK 8
+console.log(
+	function () {
+		console.log('!');
+	}
+);
