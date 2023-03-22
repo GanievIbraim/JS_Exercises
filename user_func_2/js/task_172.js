41cd448137921113e08f14e378a71969c04a2ced
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-22 17:52:19 +0300

    Global variables

---
+++ user_func_2/js/task_172.js
@@ -0,0 +1,26 @@
+//  TASK 1
+let num = 1;
+
+function func() {
+    console.log(num);
+}
+func();
+
+//  TASK 2
+let num1 = 1;
+
+function func1() {
+    console.log(num1);
+}
+num1 = 2;
+func1();
+
+//  TASK 3
+function func2() {
+    console.log(num2);
+}
+let num2;
+num2 = 1;
+func2();
+num2 = 2;
+func2();
