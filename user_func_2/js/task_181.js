f6068fc67a9acaf956a33df2587e1fce493c093e
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-03-22 18:56:57 +0300

    Function expression и Function declaration

---
+++ user_func_2/js/task_181.js
@@ -0,0 +1,18 @@
+// TASK 1
+function func() { // Function Declaration
+	console.log('!');
+}
+
+// TASK 2
+let func = function() { // Function Expression
+	console.log('!');
+}
+
+// TASK 3
+let func1 = function() {console.log('!')};
+let func2 = function() {
+	console.log('!')
+};
+function func3() {
+	console.log('!')
+}
