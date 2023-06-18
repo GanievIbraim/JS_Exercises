d273fda49d9a973119a98e1d7dc95fd0fd088e92
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-06-18 23:37:37 +0300

    Сравнение объектов с датами

---
+++ date_and time/js/task_248.js
@@ -0,0 +1,21 @@
+const now = new Date();
+const noonToday = new Date();
+noonToday.setHours(12, 0, 0, 0);
+
+if (now >= noonToday) {
+  console.log('Полдень уже был');
+} else {
+  console.log('Полдень еще не наступил');
+}
+
+
+// 2
+const now_2 = new Date();
+const midMonth = new Date();
+midMonth.setDate(15);
+
+if (now >= midMonth) {
+  console.log('Прошла уже половина месяца');
+} else {
+  console.log('Половина месяца еще не наступила');
+}
