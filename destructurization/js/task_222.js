2c725cbdef5a207d121775c54f4070ab28bba167
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-06-18 08:37:34 +0300

    Деструктуризация oбъектов параметров функций

---
+++ destructurization/js/task_222.js
@@ -0,0 +1,16 @@
+function func1({ color, width, height }) {
+  console.log('Color:', color);
+  console.log('Width:', width);
+  console.log('Height:', height);
+}
+
+func({ color: 'red', width: 400, height: 500 });
+
+// 2
+function func2({ color = 'black', width, height }) {
+  console.log('Color:', color);
+  console.log('Width:', width);
+  console.log('Height:', height);
+}
+
+func({ color: 'red', width: 400, height: 500 });
