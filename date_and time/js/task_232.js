920727fb10bf4a0bffc6cd776497485ca3b1249e
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-06-18 22:35:36 +0300

    Автоматическая корректировка дат

--- date_and time/js/task_232.js
+++ date_and time/js/task_232.js
@@ -1,11 +1,11 @@
-// 1
+// TASK 1
 // Sat Mar 03 2018 00:00:00 GMT+0300 (Moscow Standard Time).
 
-// 4
+// TASK 4
 // Sat Mar 03 2018 00:00:00 GMT+0300 (Moscow Standard Time).
 
-// 8
+// TASK 8
 // Sun Dec 31 2017 00:00:00 GMT+0300 (Moscow Standard Time).
 
-// 12
+// TASK 12
 // Tue Jan 02 2018 04:40:40 GMT+0300 (Moscow Standard Time)
