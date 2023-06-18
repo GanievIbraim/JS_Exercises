053e3a51602994c2d4adb4bb5f607ba775b9d743
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-06-19 00:46:37 +0300

    Сложные селекторы DOM элемента

---
+++ dom/js/task_252.js
@@ -0,0 +1,5 @@
+const firstParagraph = document.querySelector('#block p:first-child');
+const linkToFirstParagraph = firstParagraph.getAttribute('href');
+
+const firstParagraph2 = document.querySelector('p.www');
+const textOfFirstParagraph = firstParagraph2.textContent;
