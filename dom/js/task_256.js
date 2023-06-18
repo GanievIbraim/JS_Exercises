51f1ed099717e84443a48492a70973feb492af14
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-06-19 01:05:13 +0300

    Несколько обработчиков одного события

---
+++ dom/js/task_256.js
@@ -0,0 +1,7 @@
+// получаем элемент абзаца по id
+const elem = document.getElementById('elem');
+
+// привязываем функции к элементу
+elem.addEventListener('click', func1);
+elem.addEventListener('click', func2);
+elem.addEventListener('click', func3);
