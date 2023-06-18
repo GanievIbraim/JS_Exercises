d61f89ce362c2d16dcd73729dd9af08459f100a1
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-06-18 23:14:49 +0300

    День следующего или предыдущего месяца

---
+++ date_and time/js/task_237.js
@@ -0,0 +1,8 @@
+const today = new Date();
+const dayOfMonth = today.getDate();
+
+const oneMonthAgo = new Date(today);
+oneMonthAgo.setMonth(today.getMonth() - 1);
+oneMonthAgo.setDate(dayOfMonth);
+
+const dayOfWeek = oneMonthAgo.getDay();
