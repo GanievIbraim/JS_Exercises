ad5f3e8d6a8733fae7c5ff3149c24e4b78c61bed
GanievIbraim <ganiev.i.i.21@gmail.com>
2023-06-19 07:08:26 +0300

    Получение атрибутов

---
+++ attribute/js/task_277.js
@@ -0,0 +1,7 @@
+var elem = document.querySelector('#elem');
+var value = elem.value;
+console.log(value);
+
+var elem2 = document.querySelector('#elem2');
+var className = elem2.className;
+console.log(className);
